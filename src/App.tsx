b4199f835a095b6c52134b5345104c93e99c324a
Patrick Tomic <patrick@Patricks-MacBook-Air-3.local>
2025-03-05 14:16:55 -0500

    basic board made now

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import './style.scss'
 import './App.css'
-
-function App() {
-  const check = (array: any) => {
+let gameArray = ['','','','','','','','','']
+const winner:any = (() => {
+  const check: any = (array: any) => {
     const isTrue = false
-    const container = document.querySelector('.container')
+    const container: any = document.querySelector('.container')
     const win = [
       [0,1,2],
       [3,4,5],
@@ -22,14 +23,74 @@ function App() {
       let a = array[winCondition[0]]
       let b = array[winCondition[1]]
       let c = array[winCondition[2]]
-     
+      if(a===''|| b ===''|| c===''){
+        continue;
+      }else if(a==b && b==c){
+        let p = document.createElement('h3')
+        p.textContent='Player ' + array[winCondition[0]]+ 'is the Winner'
+        const endPoint = document.createElement('form')
+        endPoint.setAttribute('style',"display:flex;flex-direction:column;align-items:center; justify-content:center; position:absolute; width:33vw;  height:50vh; border: 1px solid black; background-color:#318CE7;");
+        const resetButton = document.createElement('button')
+        resetButton.setAttribute('type','submit')
+        resetButton.addEventListener('click',()=>{
+            
+          gameArray = ['','','','','','','','',''];
+        });
+        resetButton.textContent = 'restart';
+        endPoint.appendChild(p);
+        endPoint.appendChild(resetButton);
+       container.append(endPoint);
+      }
      }
-    
+     return {check};
   }
+    
+  })()
+ 
+ 
+  const build: any = (array: any) => {
+    let count = 0
+
+    const body = document.querySelector('body')
+    const container = document.querySelector('.container')
+    for(let i =0; i<array.length; i++){
+      const cell = document.createElement('div')
+      cell.addEventListener('mouseover',() => {
+        cell.style.background = 'grey'
+      })
+      cell.addEventListener('mouseout',() => {
+        cell.style.background = 'white;'})
+        cell.addEventListener('click',() => {
+          if(array[i] = 'X' || array[i] =='O'){
+          
+          }else{ 
+            if(count%2 === 0){
+              array.splice(i,1,'X')
+              winner.check(gameArray)
+              cell.innerHTML = array[i]
+            }else{
+              array.splice(i,1,'O')
+              winner.check(gameArray)
+              cell.innerHTML = array[i]
+            }
+          }
+        count++
+        })
+        cell.setAttribute('style','width:10vw; height:15vh;display:flex; justify-content:center; align-items:center; border:1px solid black; background-color:white; border-radius:25px;');
+        cell.innerHTML = array[i]
+        container?.appendChild(cell)
+    }
+  }
+  build(gameArray)
+ 
+function App() {
+  
+ 
+ 
   return (
     <>
     <div className='container'>
-
+     
     </div>
     </>
   )
